d51189e02e119ca61a7660aba6f64f2bcf895429
gnoirzox <gnoirzox@gmail.com>
2022-05-03 02:17:08 +0100

    move fixture at the top of the unit tests file

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,16 @@ import pytest
 import utils
 
 
+@pytest.fixture
+def words_counts_dict():
+    return {
+        "tell": 2, "the": 1, "audience": 1,
+        "what": 2, "you're": 1, "going": 1,
+        "to": 1, "say": 2, "it": 1, "then": 1,
+        "them": 1, "you've": 1, "said": 1
+    }
+
+
 def test_url_path_is_valid_true():
     assert utils.url_path_is_valid("https://www.bbc.co.uk") is True
     assert utils.url_path_is_valid("http://www.dw.com") is True
@@ -12,16 +22,6 @@ def test_url_path_is_valid_false():
     assert utils.url_path_is_valid("tps://www.bbc.co.uk") is False
     assert utils.url_path_is_valid("www.bbc.co.uk") is False
     assert utils.url_path_is_valid("www23243") is False
-
-
-@pytest.fixture
-def words_counts_dict():
-    return {
-        "tell": 2, "the": 1, "audience": 1,
-        "what": 2, "you're": 1, "going": 1,
-        "to": 1, "say": 2, "it": 1, "then": 1,
-        "them": 1, "you've": 1, "said": 1
-    }
 
 
 def test_transform_words_counts_order(words_counts_dict):
